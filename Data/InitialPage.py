eb6d5ce63afd7d64114caf66be3e3067e4a6c122
tccf1109 <tiagocarloscf@gmail.com>
2023-04-27 11:22:39 +0100

    Images

--- Data/InitialPage.py
+++ Data/InitialPage.py
@@ -1,20 +1,35 @@
 import streamlit as st
 import pandas as pd
+from PIL import Image
+import base64
+
 def page():
-    st.write("This EDA dashboard accompanies paper X Section 3.3. Please find more on the paper here")
 
-    st.write("The dashboard provides a comprehensive guide and visualization to the latest open-source "
-             "datasets related to EV's charging time series and charging flexibility behaviours. The dashboard "
-             "quickly references the dataset URLs and also explores various metrics related to these datasets.")
 
-    st.write("The following graphs and metrics are available on this dashboard for each dataset:")
+    st.write("This dashboard provides a comprehensive guide and visualization to the latest open-source datasets related to EV's charging time series and charging flexibility behaviours. The dashboard quickly references the dataset URLs and also explores various metrics related to these datasets.")
+    #centered_text = "<div style='text-align: center;'>This dashboard provides a comprehensive guide and visualization to the latest open-source datasets related to EV's charging time series and charging flexibility behaviours. The dashboard quickly references the dataset URLs and also explores various metrics related to these datasets.</div>"
+    #st.markdown(centered_text, unsafe_allow_html=True)
+
+    st.write("This dashboard accompanies paper X Section 3.3. Please find more on the paper here")
+    #centered_text = "<div style='text-align: center;'>This dashboard accompanies paper X Section 3.3. Please find more on the paper here</div>"
+    #st.markdown(centered_text, unsafe_allow_html=True)
+
+    st.write("")
+
+    #st.write("The dashboard provides a comprehensive guide and visualization to the latest open-source "
+    #         "datasets related to EV's charging time series and charging flexibility behaviours. The dashboard "
+    #         "quickly references the dataset URLs and also explores various metrics related to these datasets.")
+
+    #with st.expander("Learn More abou the dashboard:"):
+
+    st.write("The following graphs and metrics are may be available for each dataset:")
 
     st.write(
         "- A histogram that shows the total number of charging hours per session.")
 
     st.write(
         "- A graph that shows the total number of charging sessions per amount of charging time, presented as two "
-        "lines - one for weekdays and another for weekends")
+        "lines - one for weekdays and another for weekends, or as a different graph for each day of the week")
 
     st.write(
         "- A graph that shows the number of charging sessions for each 15-minute interval of arriving and departing "
@@ -27,3 +42,34 @@ def page():
     st.write("- A histogram of the energy supplied and the number of events that supplied that energy.")
 
     st.write("- Other metrics might apply given the specific data present at each dataset")
+
+
+
+def show_logo(link, path, width, padding, margin):
+    padding_top, padding_right, padding_bottom, padding_left = padding
+    margin_top, margin_right, margin_bottom, margin_left = margin
+
+    link = link
+
+    with open(path, "rb") as f:
+        data = f.read()
+
+    bin_str = base64.b64encode(data).decode()
+    html_code = f"""
+        <a href="{link}" target = _blank>
+            <img src="data:image/png;base64,{bin_str}"
+            style="
+                 margin: auto;
+                 width: {width}%;
+                 margin-top: {margin_top}px;
+                 margin-right: {margin_right}px;
+                 margin-bottom: {margin_bottom}px;
+                 margin-left: {margin_left}%;
+                 padding-top: {margin_top}px;
+                 padding-right: {padding_right}px;
+                 padding-bottom: {padding_bottom}px;
+                 padding-left: {padding_left}%;
+                 "/>
+        </a>"""
+
+    return html_code
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit as st
+from PIL import Image
+import base64
 
 st.set_page_config(
-    page_title="EV EDA",
+    page_title="EV Data Visualization",
     page_icon="🚗",
     initial_sidebar_state="auto",
     menu_items=None,
@@ -12,8 +14,54 @@ from Data import (PecanStreetviz, Residential_electric_vehicle_charging_datasets
                   Energy_consumption_and_renewable_generation_data_of_5_aggregators, PerthKinross, Boulder, Dundee,
                   EletricChargepoint, ElaadNL, PaloAlto, ACN_Data, ICharging, InitialPage)
 
+st.markdown("""
+<style>
+.centered {
+    display: block;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+}
+</style>
+""", unsafe_allow_html=True)
 
-st.title("EVs' Datasets Exploratory Data Analysis 🚗🔋⚡🔌")
+st.markdown("<h1 class='centered'>EVs' Datasets Data Visualization</h1>", unsafe_allow_html=True)
+
+# Load your images (local file paths or URLs)
+image1_path = "./Data/Images/logo-opeva.jpg"
+image2_path = "./Data/Images/2023_logo.png"
+
+# Load images using PIL
+image1 = Image.open(image1_path)
+image2 = Image.open(image2_path)
+
+# Determine the smaller dimensions of the two images
+width = min(image1.width, image2.width)
+height = min(image1.height, image2.height)
+
+# Resize the images to the smaller dimensions
+image1_resized = image1.resize((width, height))
+image2_resized = image2.resize((width, height))
+
+logoOpeva = InitialPage.show_logo("https://opeva.eu/", image1_path,
+        width=80, padding=[0, 0, 0, 0], margin=[0, 0, 0, 0]
+    )
+logoisep = InitialPage.show_logo("https://www.isep.ipp.pt/", image2_path,
+        width=105, padding=[0, 0, 0, 0], margin=[0, 0, 0, 0]
+    )
+
+# Create two columns and add a single container for centering
+left_column, center_column, right_column = st.columns([1, 2, 1])
+with center_column:
+    # Create two columns for images
+    col1, col2 = st.columns(2)
+
+    # Display images side by side
+    col1.markdown(logoOpeva, unsafe_allow_html=True)
+    col2.markdown(logoisep, unsafe_allow_html=True)
+
+    #col1.image(image1_resized, use_column_width=True)
+    #col2.image(image2_resized, use_column_width=True)
 
 
 # specify the path of the directory
@@ -59,6 +107,32 @@ else:
     st.error("The dataset you selected could not be found on our database")
 
 
+st.write("________________________________________________________________________________________")
 
+image3_path = "./Data/Images/EU.png"
+
+# Create two columns and add a single container for centering
+st.subheader("This work was funded by European Project:")
+
+left1_column, right1_column = st.columns([3, 1])
+with left1_column:
+    link = "https://opeva.eu/"
+    text = "OPEVA - OPtimization of Electric Vehicule Autonomy "
+    st.markdown(f"[{text}]({link})", unsafe_allow_html=True)
+    st.write("Funded by Key Digital Technologies Joint Undertaking (KDT JU) from the European Union’s Horizon Europe Programme and the National Authorities, under grant agreement 101097267.")
+with right1_column:
+    st.image(image3_path)
+
+st.write("________________________________________________________________________________________")
+
+left2_column, right2_column = st.columns([4, 2])
+with left2_column:
+    st.write("Created by: Tiago Fonseca")
+    st.write("Software for Cyber Physical Systems (SoftCPS) Research Group")
+with right2_column:
+    logo = InitialPage.show_logo("https://github.com/calofonseca/EVDatasetsVizualization", "./Data/Images/128-1280187_github-logo-png-github-transparent-png.png",
+        width=65, padding=[0, 6, 20, 25], margin=[0, 0, 30, 0]
+    )
+    st.markdown(logo, unsafe_allow_html=True)
 
 
