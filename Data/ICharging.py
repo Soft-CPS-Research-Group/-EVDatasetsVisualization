44f069c0e4cd322746111d0eeaee1d54622a5405
tccf1109 <tiagocarloscf@gmail.com>
2023-04-27 09:31:45 +0100

    I-Charging

--- Data/ICharging.py
+++ Data/ICharging.py
@@ -1,51 +1,38 @@
 import streamlit as st
 import pandas as pd
-import plotly.graph_objs as go
+import plotly.express as px  # Import Plotly Express
 from datetime import datetime, date, timedelta
-import matplotlib.pyplot as plt
 
-# Function to create visualizations
-def create_visualizations(data):
+
+# Update the function to accept the selected serial number
+def create_visualizations(data, selected_serial_number):
     # Filter the data for charging events
     charging_data = data[data['status'] == 10]
 
-    # Plot energy vs time
-    fig1, ax1 = plt.subplots(figsize=(10, 6))
-    ax1.scatter(charging_data['localtime (s)'], charging_data['energy (kWh)'])
-    ax1.set_xlabel('Charging Time (s)')
-    ax1.set_ylabel('Energy (kWh)')
-    ax1.set_title('Energy vs Charging Time')
-    ax1.grid(True)
+    # Filter the data by the selected serial number
+    charging_data = charging_data[charging_data['serialnumber'] == selected_serial_number]
 
-    # Plot power vs time
-    fig2, ax2 = plt.subplots(figsize=(10, 6))
-    ax2.scatter(charging_data['localtime (s)'], charging_data['power (kW)'])
-    ax2.set_xlabel('Charging Time (s)')
-    ax2.set_ylabel('Power (kW)')
-    ax2.set_title('Power vs Charging Time')
-    ax2.grid(True)
+    # Find the rows where the value of 'soc' decreases
+    mask = charging_data['soc (0-100%)'].diff() < 0
 
-    # Plot SOC vs time
-    fig3, ax3 = plt.subplots(figsize=(10, 6))
-    ax3.scatter(charging_data['localtime (s)'], charging_data['soc (0-100%)'])
-    ax3.set_xlabel('Charging Time (s)')
-    ax3.set_ylabel('State of Charge (0-100%)')
-    ax3.set_title('State of Charge vs Charging Time')
-    ax3.grid(True)
+    # Create a new column 'event_id' and increment the ID each time there's a decrease in 'soc'
+    charging_data['event_id'] = mask.cumsum()
 
-    # Count charging events per connector type
-    connector_counts = charging_data['type (CCS, CHAdeMO)'].value_counts()
+    # Label the events with a unique ID
+    charging_data['event_id'] = 'event_' + charging_data['event_id'].astype(str)
 
-    # Plot connector type distribution
-    fig4, ax4 = plt.subplots(figsize=(6, 6))
-    connector_counts.plot(kind='bar', ax=ax4)
-    ax4.set_xlabel('Connector Type')
-    ax4.set_ylabel('Number of Charging Events')
-    ax4.set_title('Connector Type Distribution')
-    ax4.set_xticklabels(['CCS1', 'CCS2', 'CHAdeMO'])
-    ax4.grid(axis='y')
+    serial_numbers = charging_data['event_id'].unique()
+    selected_event_id = st.selectbox('Select a charging event:', sorted(serial_numbers))
 
-    return fig1, fig2, fig3, fig4
+    filtered_charging_data = charging_data[charging_data['event_id'] == selected_event_id]
+
+    # Create Plotly Express visualizations
+    fig1 = px.scatter(filtered_charging_data, x='localtime (s)', y='energy (kWh)', title='Energy vs Charging Time')
+    fig2 = px.scatter(filtered_charging_data, x='localtime (s)', y='power (kW)', title='Power vs Charging Time')
+    fig3 = px.scatter(filtered_charging_data, x='soc (0-100%)', y='power (kW)', title='State of Charge vs Power')
+
+    return fig1, fig2, fig3
+
 
 def page():
     st.subheader("You have selected the I-Charging dataset")
@@ -55,19 +42,23 @@ def page():
     uploaded_file = st.file_uploader("Upload your CSV file", type=["csv"])
 
     if uploaded_file is not None:
-        data = pd.read_csv(uploaded_file, sep=";", index_col=False)
-        fig1, fig2, fig3, fig4 = create_visualizations(data)
+        data = pd.read_csv(uploaded_file, sep=";")
+
+        # Add a Streamlit widget to let users choose the serial number
+        serial_numbers = data['serialnumber'].unique()
+        selected_serial_number = st.selectbox('Select a serial number:', sorted(serial_numbers))
+
+        # Pass the selected serial number to the function
+        fig1, fig2, fig3 = create_visualizations(data, selected_serial_number)
 
         st.header("Energy vs Charging Time")
-        st.pyplot(fig1)
+        st.plotly_chart(fig1)
 
         st.header("Power vs Charging Time")
-        st.pyplot(fig2)
+        st.plotly_chart(fig2)
 
         st.header("State of Charge vs Charging Time")
-        st.pyplot(fig3)
+        st.plotly_chart(fig3)
 
-        st.header("Connector Type Distribution")
-        st.pyplot(fig4)
     else:
         st.write("Please upload a CSV file to visualize the data.")
